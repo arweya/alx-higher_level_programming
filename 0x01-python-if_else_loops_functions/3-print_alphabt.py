317a8b2f9090a600e1ea317f3d337f995b258eca
arweya <anitarueya@gmail.com>
2022-11-30 07:54:12 -0800

    Update 3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for letter in range(97, 123): 
-    if chr(letter) == 'q' or chr(letter) == 'e'
+for letter in range(97, 123):
+    if chr(letter) == 'q' or chr(letter) == 'e':
         continue
-     print('{:c}'.format(letter), end='')
+    print("{:c}".format(ch), end='')
