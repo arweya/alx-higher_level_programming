b7d0e56b5f1b2b234098b583c76bd8ea7aa9d0cb
arweya <anitarueya@gmail.com>
2023-01-11 05:23:25 -0800

    Update 1-write_file.py

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Write a function that writes a string to a text file (UTF8) and returns the number of characters written:"""
+
+def write_file(filename="", text=""):
+    """Function that writes into a file and create the file if not existing, returning the num of char."""
+    with open(filename, mode="w", encoding="utf8") as file:
+        return file.write(text)
